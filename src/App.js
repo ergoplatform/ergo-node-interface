735ce989eceb88c4e28506c29e26d6271fea5376
Deadit <stdeadit@gmail.com>
2019-09-03 11:46:19 +0300

    initialize wallet view

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Layout from './components/layout'
 import Dashboard from './components/pages/Dashboard'
+import Wallet from './components/pages/Wallet'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Layout>
         <Switch>
           <Route exact path="/" component={Dashboard} />
+          <Route exact path="/wallet" component={Wallet} />
         </Switch>
       </Layout>
     </Router>
