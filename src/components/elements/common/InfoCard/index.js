5f41dc2939ddf74894fe0e8a918b6e2f557a793f
Deadit <stdeadit@gmail.com>
2019-09-03 11:44:29 +0300

    update styles on dashboard page

---
+++ src/components/elements/common/InfoCard/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import clsx from 'clsx'
+import './index.scss'
+
+const InfoCard = ({ color, children, className }) => {
+  return (
+    <div
+      className={clsx(
+        {
+          'info-card': true,
+          'info-card--green': color === 'green',
+          'info-card--orange': color === 'orange',
+        },
+        className,
+      )}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default InfoCard
