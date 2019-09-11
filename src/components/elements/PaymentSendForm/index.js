9ae65bb8ab02fdd91b34a39edfcf05df55443f80
Deadit <stdeadit@gmail.com>
2019-09-11 13:43:53 +0300

    PaymentSendForm init

--- src/components/elements/PaymentSendForm/index.js
+++ src/components/elements/PaymentSendForm/index.js
@@ -1,70 +1,106 @@
 import React, { Component } from 'react'
+import { Formik, Field, Form } from 'formik'
+import { successToast } from '../../../utils/toast'
+import nodeApi from '../../../api/api'
 
-export class PaymentSendForm extends Component {
+class PaymentSendForm extends Component {
+  paymentSend = async () => {
+    await nodeApi.post(
+      '/wallet/unlock',
+      {},
+      {
+        headers: {
+          api_key: '',
+        },
+      },
+    )
+  }
+
   render() {
     return (
       <div className="col-4">
-        <form className="card bg-white p-4">
+        <div className="card bg-white p-4">
           <h2 className="h5 mb-3">Payment Send Form</h2>
-          <div className="form-group">
-            <label for="apiKeyInput">API key</label>
-            <input
-              type="text"
-              className="form-control"
-              id="apiKeyInput"
-              aria-describedby="apiKey"
-              placeholder="Enter API key"
-            />
-            <small id="apiKeyHelp" className="form-text text-muted">
-              Enter api key here, <b>not api key hash</b>
-            </small>
-          </div>
-          <div className="form-group">
-            <label for="exampleInputPassword1">Wallet password *</label>
-            <input
-              type="text"
-              className="form-control"
-              id="exampleInputPassword1"
-              placeholder="Password"
-            />
-            <small id="apiKeyHelp" className="form-text text-muted">
-              * If you have it <b>or leave field empty</b>
-            </small>
-          </div>
-          <div className="form-group">
-            <label for="exampleInputPassword1">Recipient address</label>
-            <input
-              type="text"
-              className="form-control"
-              id="exampleInputPassword1"
-              placeholder="Enter recipient address"
-            />
-          </div>
-          <div className="form-group">
-            <label for="exampleInputPassword1">Amount</label>
-            <input
-              type="text"
-              className="form-control"
-              id="exampleInputPassword1"
-              placeholder="Enter amount"
-            />
-          </div>
-          {/* <div className="form-group">
-            <label for="exampleInputPassword1">Fee *</label>
-            <input
-              type="text"
-              className="form-control"
-              id="exampleInputPassword1"
-              value="1000000"
-            />
-            <small id="apiKeyHelp" className="form-text text-muted">
-              <b>* Optional. Default 1.000.000 mErgs</b>
-            </small>
-          </div> */}
-          <button type="submit" className="btn btn-primary">
-            Send
-          </button>
-        </form>
+          <Formik
+            initialValues={{
+              apiKey: '',
+              walletPassword: '',
+              recipientAddress: '',
+              amount: 0,
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              if (window.confirm('Are you sure ?')) {
+                this.paymentSend()
+                successToast('Sended')
+                setSubmitting(false)
+              }
+            }}
+          >
+            {({ isSubmitting }) => (
+              <Form>
+                <div className="form-group">
+                  <label htmlFor="api-key-input">API key</label>
+                  <Field
+                    name="apiKey"
+                    type="text"
+                    id="api-key-input"
+                    className="form-control"
+                    aria-describedby="apiKey"
+                    placeholder="Enter API key"
+                  />
+                  <small id="apiKeyHelp" className="form-text text-muted">
+                    Enter api key here, <b>not api key hash</b>
+                  </small>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="wallet-password-input">
+                    Wallet password *
+                  </label>
+                  <Field
+                    name="walletPassword"
+                    type="password"
+                    id="wallet-password-input"
+                    className="form-control"
+                    placeholder="Enter wallet password"
+                  />
+                  <small
+                    id="walletPasswordHelp"
+                    className="form-text text-muted"
+                  >
+                    * If you have it <b>or leave field empty</b>
+                  </small>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="recipient-address">Recipient address</label>
+                  <Field
+                    type="text"
+                    name="recipientAddress"
+                    id="recipient-address-input"
+                    className="form-control"
+                    placeholder="Enter recipient address"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="amount">Amount</label>
+                  <Field
+                    type="text"
+                    name="amount"
+                    id="amount-input"
+                    className="form-control"
+                    placeholder="Enter amount"
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="btn btn-primary"
+                  disabled={isSubmitting}
+                >
+                  Send
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </div>
       </div>
     )
   }
--- src/components/pages/Wallet/index.js
+++ src/components/pages/Wallet/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import PaymentSendForm from '../../elements/PaymentSendForm'
 
 export default () => {
   return (
     <div className="container-fluid mt-4">
-      <div className="row"></div>
+      <div className="row">
+        <PaymentSendForm />
+      </div>
     </div>
   )
 }
