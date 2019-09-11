b67abdc4543c7b18a6b149fd3b6a5ce2c699a4e2
Deadit <stdeadit@gmail.com>
2019-09-11 13:40:13 +0300

    wallet page init

--- src/components/pages/Wallet/index.js
+++ src/components/pages/Wallet/index.js
@@ -1,14 +1,9 @@
-import React, { Component } from 'react'
-import PaymentSendForm from '../../elements/PaymentSendForm'
+import React from 'react'
 
-export default class Wallet extends Component {
-  render() {
-    return (
-      <div className="container-fluid mt-4">
-        <div className="row">
-          <PaymentSendForm />
-        </div>
-      </div>
-    )
-  }
+export default () => {
+  return (
+    <div className="container-fluid mt-4">
+      <div className="row"></div>
+    </div>
+  )
 }
