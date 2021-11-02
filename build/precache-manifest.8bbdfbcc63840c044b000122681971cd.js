4b4a54f56305c23b05b25f236ff4403839e22bc0
deadit <stdeadit@gmail.com>
2021-11-02 17:44:52 +0300

    add wallet error to the dashboard

--- build/precache-manifest.8bbdfbcc63840c044b000122681971cd.js
+++ build/precache-manifest.8bbdfbcc63840c044b000122681971cd.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "889f9d040ee3c0d53b245bc9735e5dda",
+    "revision": "2b8e86a022c77f6ec346624b01480771",
     "url": "/index.html"
   },
   {
-    "revision": "e876be637300bb1b4498",
+    "revision": "958afc9417af1de07489",
     "url": "/static/css/2.9338f6a1.chunk.css"
   },
   {
-    "revision": "99b2b818b082287d9504",
+    "revision": "1b9da9389a26f9bce4bd",
     "url": "/static/css/main.0e9161bb.chunk.css"
   },
   {
-    "revision": "e876be637300bb1b4498",
-    "url": "/static/js/2.37d76b6f.chunk.js"
+    "revision": "958afc9417af1de07489",
+    "url": "/static/js/2.6b84a7b0.chunk.js"
   },
   {
-    "revision": "99b2b818b082287d9504",
-    "url": "/static/js/main.e40fcaee.chunk.js"
+    "revision": "1b9da9389a26f9bce4bd",
+    "url": "/static/js/main.ca050acc.chunk.js"
   },
   {
     "revision": "ce813ebd69754efed759",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.515a15cf1491d86f6bac93934532adc4.js"
+  "/precache-manifest.8bbdfbcc63840c044b000122681971cd.js"
 );
 
 self.addEventListener('message', (event) => {
