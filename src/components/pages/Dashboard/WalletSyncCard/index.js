f57c048f13c6c49dd6b3cbeeb648519e4b7dd4c6
deadit <stdeadit@gmail.com>
2021-11-02 17:43:11 +0300

    updates

--- src/components/pages/Dashboard/WalletSyncCard/index.js
+++ src/components/pages/Dashboard/WalletSyncCard/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSync, faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faSync, faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
 import InfoCard from '../InfoCard';
 
 export default class WalletSyncCard extends Component {
@@ -22,13 +22,27 @@ export default class WalletSyncCard extends Component {
     </>
   );
 
+  renderErrorSynchronization = () => (
+    <>
+      <p className="info-card__title">Synchronization state</p>
+      <p className="info-card__label text-danger">
+        <FontAwesomeIcon icon={faTimes} /> Error - Something happend with the node.
+      </p>
+    </>
+  );
+
   renderSynchronizationState = (state) =>
     ({
       active: this.renderActiveSynchronization,
       complete: this.renderCompleteSynchronization,
+      error: this.renderErrorSynchronization,
     }[state]);
 
   getSynchronizationState = (walletStatusData, headersHeight) => {
+    if (walletStatusData.error?.trim().length !== 0) {
+      return 'error';
+    }
+
     if (
       walletStatusData.walletHeight !== null &&
       headersHeight !== null &&
